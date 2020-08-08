75201d2ff12cbea4253f0b0e396c26b3f44fdb1b
Satriadinata <satriadinata2@gmail.com>
2020-08-08 22:36:39 +0700

    Initial commit

---
+++ app/src/main/java/com/sdev/mov/OnBoardOneActivity.kt
@@ -0,0 +1,11 @@
+package com.sdev.mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board_one)
+    }
+}
---
+++ app/src/main/java/com/sdev/mov/OnBoardThreeActivity.kt
@@ -0,0 +1,11 @@
+package com.sdev.mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board_three)
+    }
+}
